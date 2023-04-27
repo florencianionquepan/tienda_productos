09bbb67b71596b1ec11e9f4846baecfece945739
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-27 20:31:06 -0300

    Crea funciones estaticas para obtener producto por producto
    Estas seran utilizadas por VentaLoader para crear sus objetos

--- src/database/ProductoLoader.java
+++ src/database/ProductoLoader.java
@@ -21,4 +21,36 @@ public class ProductoLoader {
                 )
         );
     }
+
+    public static Producto getProductoUno(){
+        return new Producto(1L, "P001", "Laptop HP", 15000.0f, "Electronica"),
+    }
+
+    public static Producto getProductoDos(){
+        return new Producto(2L, "P002", "Smartphone Samsung", 8000.0f, "Electronica");
+    }
+
+    public static Producto getProductoTres(){
+        return new Producto(3L, "P003", "Televisor LG", 12000.0f, "Electronica");
+    }
+
+    public static Producto getProductoCuatro(){
+        return new Producto(4L, "P004", "Zapatillas Nike", 2000.0f, "Calzado");
+    }
+
+    public static Producto getProductoCinco(){
+        return new Producto(5L, "P005", "Jeans Levi's", 1500.0f, "Ropa");
+    }
+
+    public static Producto getProductoSeis(){
+        return new Producto(6L, "P006", "Reloj Casio", 1000.0f, "Ropa");
+    }
+
+    public static Producto getProductoSiete(){
+        return new Producto(7L, "P007", "Botella de vino tinto", 500.0f, "Bebidas");
+    }
+
+    public static Producto getProductoOcho(){
+        return new Producto(8L, "P008", "Caja de chocolates", 200.0f, "Dulces");
+    }
 }
