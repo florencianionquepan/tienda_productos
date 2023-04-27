f000f7157b83a09d510769b000a5c61593f0570e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-27 19:58:11 -0300

    Create VendedorRepoMemo.java

---
+++ src/repository/memoria/VendedorRepoMemo.java
@@ -0,0 +1,34 @@
+package repository.memoria;
+
+import entidad.Vendedor;
+
+import java.util.List;
+import java.util.Optional;
+
+public class VendedorRepoMemo implements IVendedorRepoMemo{
+
+    public final List<Vendedor> vendedores;
+
+    public VendedorRepoMemo(List<Vendedor> vendedores) {
+        this.vendedores = vendedores;
+    }
+
+    @Override
+    public Vendedor crear(Vendedor vendedor) {
+        vendedor.setId((long) (this.vendedores.size()+1));
+        return vendedor;
+    }
+
+    @Override
+    public List<Vendedor> verTodos() {
+        return this.vendedores;
+    }
+
+    @Override
+    public Optional<Vendedor> buscarByCodigo(String codigo) {
+        Optional<Vendedor> oVende=this.vendedores.stream()
+                .filter(vende->vende.getCodigo().equals(codigo))
+                .findAny();
+        return oVende;
+    }
+}
