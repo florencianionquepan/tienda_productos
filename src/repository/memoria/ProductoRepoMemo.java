54d7450b1acd73622fb218964d1132ede27ea9ff
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-27 19:41:54 -0300

    Update ProductoRepoMemo.java
    Se accede a lista precargada de productos mediante clase que simula una db

--- src/repository/memoria/ProductoRepoMemo.java
+++ src/repository/memoria/ProductoRepoMemo.java
@@ -10,22 +10,13 @@ import java.util.stream.Collectors;
 
 public class ProductoRepoMemo implements IProductoRepoMemo{
 
-    private List<Producto> productos;
+    public final List<Producto> productos;
 
     public ProductoRepoMemo(List<Producto> productos) {
-        this.productos = new ArrayList<Producto>(
-                Arrays.asList(
-                        new Producto(1L, "P001", "Laptop HP", 15000.0f, "Electronica"),
-                        new Producto(2L, "P002", "Smartphone Samsung", 8000.0f, "Electronica"),
-                        new Producto(3L, "P003", "Televisor LG", 12000.0f, "Electronica"),
-                        new Producto(4L, "P004", "Zapatillas Nike", 2000.0f, "Calzado"),
-                        new Producto(5L, "P005", "Jeans Levi's", 1500.0f, "Ropa"),
-                        new Producto(6L, "P006", "Reloj Casio", 1000.0f, "Ropa"),
-                        new Producto(7L, "P007", "Botella de vino tinto", 500.0f, "Bebidas"),
-                        new Producto(8L, "P008", "Caja de chocolates", 200.0f, "Dulces")
-                )
-        );
+        this.productos=productos;
     }
+
+
 
     @Override
     public List<Producto> verTodos() {
