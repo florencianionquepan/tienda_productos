a8e05ac9040bf9abce6802bf3e6708f717153952
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-27 19:50:34 -0300

    Create VendedorLoader.java

---
+++ src/database/VendedorLoader.java
@@ -0,0 +1,17 @@
+package database;
+
+import entidad.Vendedor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class VendedorLoader {
+    public static List<Vendedor> cargarVende(){
+        return new ArrayList<Vendedor>(
+                Arrays.asList(new Vendedor(1L, "V001", "Juan Perez", 15000.0f),
+                            new Vendedor(2L, "V002", "Ana Gomez", 12000.0f)
+                )
+        );
+    }
+}
