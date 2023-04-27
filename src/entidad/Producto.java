00bb32998e3ad5e8626c8de0013f23c81c68c920
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-27 17:23:35 -0300

    Create Producto.java

---
+++ src/entidad/Producto.java
@@ -0,0 +1,71 @@
+package entidad;
+
+public class Producto {
+    private Long id;
+    private String codigo;
+    private String nombre;
+    private float precio;
+    private String categoria;
+
+    public Producto() {
+    }
+
+    public Producto(Long id, String codigo, String nombre, Double precio, String categoria) {
+        this.id = id;
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", categoria='" + categoria + '\'' +
+                '}';
+    }
+}
