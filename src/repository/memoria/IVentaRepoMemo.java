ec484c0aed8238ff96e43fcaf871037833993004
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-27 21:08:30 -0300

    Create IVentaRepoMemo.java

---
+++ src/repository/memoria/IVentaRepoMemo.java
@@ -0,0 +1,13 @@
+package repository.memoria;
+
+import entidad.Producto;
+import entidad.Vendedor;
+import entidad.Venta;
+
+import java.util.List;
+
+public interface IVentaRepoMemo {
+    Venta crear(Venta nueva);
+    List<Venta> listarByVendedor(Vendedor vendedor);
+    List<Venta> listarByProducto(Producto producto);
+}
