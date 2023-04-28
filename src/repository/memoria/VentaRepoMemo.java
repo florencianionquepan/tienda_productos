1be242d29d27d1e3b96ba354857305a07cca065a
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-27 21:08:34 -0300

    Create VentaRepoMemo.java

---
+++ src/repository/memoria/VentaRepoMemo.java
@@ -0,0 +1,40 @@
+package repository.memoria;
+
+import entidad.Producto;
+import entidad.Vendedor;
+import entidad.Venta;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Nodes.collect;
+
+public class VentaRepoMemo implements IVentaRepoMemo{
+
+    public final List<Venta> ventas;
+
+    public VentaRepoMemo(List<Venta> ventas) {
+        this.ventas = ventas;
+    }
+
+    @Override
+    public Venta crear(Venta nueva) {
+        nueva.setId((long) (this.ventas.size()+1));
+        return nueva;
+    }
+
+    @Override
+    public List<Venta> listarByVendedor(Vendedor vendedor) {
+        return this.ventas.stream()
+                .filter(venta->venta.getVendedor().getCodigo().equals(vendedor.getCodigo()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Venta> listarByProducto(Producto producto) {
+        return this.ventas.stream()
+                .filter(venta->venta.getProductos().stream()
+                        .anyMatch(p->p.getCodigo().equals(producto.getCodigo())))
+                .collect(Collectors.toList());
+    }
+}
