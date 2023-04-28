13197ae3abbe3497c254ae224bd343feea3fe2ec
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-28 15:22:23 -0300

    Se agregan atributos cantidad y lista de ventas en Producto
    Se modifican las relaciones entre producto y venta.

--- src/entidad/Producto.java
+++ src/entidad/Producto.java
@@ -1,21 +1,29 @@
 package entidad;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Producto {
     private Long id;
     private String codigo;
     private String nombre;
     private float precio;
+    private int cantidad;
     private String categoria;
+    private List<Venta> ventas;
 
     public Producto() {
     }
 
-    public Producto(Long id, String codigo, String nombre, float precio, String categoria) {
+    public Producto(Long id, String codigo, String nombre, float precio,
+                    String categoria, int cantidad, List<Venta> ventas) {
         this.id = id;
         this.codigo = codigo;
         this.nombre = nombre;
         this.precio = precio;
         this.categoria = categoria;
+        this.cantidad = cantidad;
+        this.ventas = ventas;
     }
 
     public Long getId() {
@@ -58,14 +66,40 @@ public class Producto {
         this.categoria = categoria;
     }
 
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public List<Venta> getVentas() {
+        return ventas;
+    }
+
+    public void setVentas(List<Venta> ventas) {
+        this.ventas = ventas;
+    }
+
     @Override
     public String toString() {
-        return "Producto{" +
-                "id=" + id +
-                ", codigo='" + codigo + '\'' +
-                ", nombre='" + nombre + '\'' +
-                ", precio=" + precio +
-                ", categoria='" + categoria + '\'' +
-                '}';
+        StringBuilder sb = new StringBuilder();
+        sb.append("Producto{id=").append(id);
+        sb.append(", codigo='").append(codigo).append('\'');
+        sb.append(", nombre='").append(nombre).append('\'');
+        sb.append(", precio=").append(precio);
+        sb.append(", categoria='").append(categoria).append('\'');
+        sb.append(", ventas=");
+        if (ventas.isEmpty()) {
+            sb.append("[]");
+        } else {
+            sb.append("[");
+            sb.append(ventas.stream().map(v -> String.valueOf(v.getId()))
+                    .collect(Collectors.joining(", ")));
+            sb.append("]");
+        }
+        sb.append("}");
+        return sb.toString();
     }
 }
