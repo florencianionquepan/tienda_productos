b0d33c4285abb96546831e205bce65bc605d2a32
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-28 21:31:04 -0300

    Simplifica ProductoLoader

--- src/database/ProductoLoader.java
+++ src/database/ProductoLoader.java
@@ -11,54 +11,40 @@ public class ProductoLoader {
     public static List<Producto> cargarProductos(){
         return new ArrayList<Producto>(
                 Arrays.asList(
-                        new Producto(1L, "P001", "Laptop HP", 15000.0f,
-                                "Electronica",5, new ArrayList<Venta>()),
-                        new Producto(2L, "P002", "Smartphone Samsung", 8000.0f,
-                                "Electronica",3,new ArrayList<Venta>()),
-                        new Producto(3L, "P003", "Televisor LG", 12000.0f,
-                                "Electronica",5,new ArrayList<Venta>()),
-                        new Producto(4L, "P004", "Zapatillas Nike", 2000.0f,
-                                "Calzado",2,new ArrayList<Venta>()),
+                        getProductoUno(),
+                        getProductoDos(),
+                        getProductoTres(),
+                        getProductoCuatro(),
                         new Producto(5L, "P005", "Jeans Levi's", 1500.0f,
-                                "Ropa",2,new ArrayList<Venta>()),
+                                "Ropa",2),
                         new Producto(6L, "P006", "Reloj Casio", 1000.0f,
-                                "Ropa",1,new ArrayList<Venta>()),
+                                "Ropa",1),
                         new Producto(7L, "P007", "Botella de vino tinto", 500.0f,
-                                "Bebidas",3,new ArrayList<Venta>()),
+                                "Bebidas",3),
                         new Producto(8L, "P008", "Caja de chocolates", 200.0f,
-                                "Dulces",0,new ArrayList<Venta>())
+                                "Dulces",0)
                 )
         );
     }
 
     public static Producto getProductoUno(){
         return new Producto(1L, "P001", "Laptop HP", 15000.0f,
-                "Electronica",5, new ArrayList<Venta>());
+                "Electronica",5);
     }
 
     public static Producto getProductoDos(){
         return new Producto(2L, "P002", "Smartphone Samsung", 8000.0f,
-                "Electronica",3,new ArrayList<Venta>());
+                "Electronica",3);
     }
 
     public static Producto getProductoTres(){
         return new Producto(3L, "P003", "Televisor LG", 12000.0f,
-                "Electronica",5,new ArrayList<Venta>());
+                "Electronica",5);
     }
 
     public static Producto getProductoCuatro(){
         return new Producto(4L, "P004", "Zapatillas Nike", 2000.0f,
-                "Calzado",2,new ArrayList<Venta>());
-    }
-
-    public static Producto getProductoCinco(){
-        return new Producto(5L, "P005", "Jeans Levi's", 1500.0f,
-                "Ropa",2,new ArrayList<Venta>());
-    }
-
-    public static Producto getProductoSeis(){
-        return new Producto(6L, "P006", "Reloj Casio", 1000.0f,
-                "Ropa",1,new ArrayList<Venta>());
+                "Calzado",2);
     }
 
 }
